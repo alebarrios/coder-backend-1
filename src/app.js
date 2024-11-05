6de690a6d5871089aa448b9a95f1361f2ff83a40
Ale Barrios <al.barrios88@gmail.com>
2024-11-05 19:58:24 -0300

    Servidor con Express + Estructura de carpetas

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req,res) => {
+    res.send("<h1>Hello World</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+});
