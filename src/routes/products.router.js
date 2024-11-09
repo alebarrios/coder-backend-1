311142d098bbcb1a1496848dd0d8b764ff07f7ef
Ale Barrios <al.barrios88@gmail.com>
2024-11-09 19:47:41 -0300

    Add controles de error en GET productos 500/404

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,14 +7,32 @@ const productManager = new ProductManager();
 productsRouter.get('/', getAllProducts)
 productsRouter.get('/:id', getProductById)
 
+productsRouter.post('/', postProduct)
+
 //inversion de control. Lo uso para poder hacer el Testing lo mas desacoplado posible.
 export default (app) => { app.use('/api/products', productsRouter)}
 
 async function getAllProducts(req,res){
-    res.status(200).json(await productManager.getAll());
+    try {
+        const products = await productManager.getAll();
+        res.status(200).json({ status: "success", payload: products });
+
+    } catch (error) {
+        res.status(error.code || 500).json({ status: "error", message: error.message });
+    }
 };
 
 async function getProductById(req,res){
-    const { id } = req.params;
-    res.status(200).json(await productManager.getOneById(id));
+    try {
+        const { id } = req.params;
+        const product = await productManager.getOneById(id);
+        res.status(200).json({ status: "success", payload: product });
+
+    } catch (error) {
+        res.status(error.code || 500).json({ status: "error", message: error.message });
+    }
+};
+
+async function postProduct(req,res){
+    res.status(201).json(await productManager.getAll());
 };
--- src/routes/products.router.test.js
+++ src/routes/products.router.test.js
@@ -4,6 +4,7 @@ import productsRouter from "./products.router.js";
 const request = testServer(productsRouter)
 
 describe('GET /api/products', () => {
+
     it("should return 200", async () => {
         const { status } = await request.get('/api/products');
         expect(status).toEqual(200);
@@ -11,17 +12,31 @@ describe('GET /api/products', () => {
 
     it("should return 200", async () => {
         const { status, body } = await request.get('/api/products');
-        //console.log(body);
         expect(status).toEqual(200);
-        expect(body.length).toEqual(3);
+        expect(body.status).toBe("success");
+        expect(body.payload.length).toEqual(3);
     });
 });
 
 describe('GET /api/products/:id', () => {
+
     it("should return 200", async () => {
-        const { status, body : product } = await request.get('/api/products/2');
+        const { status, body } = await request.get('/api/products/2');
         expect(status).toEqual(200);
-        expect(product.id).toEqual(2);
+        expect(body.status).toBe("success");
+        expect(body.payload.id).toEqual(2);
+    });
+
+    it("should return 404", async () => {
+        const { status, body } = await request.get('/api/products/867');
+        expect(status).toEqual(404);
+        expect(body.status).toEqual("error");
+    });
+
+    it("should return 404", async () => {
+        const { status, body } = await request.get('/api/products/bla');
+        expect(status).toEqual(404);
+        expect(body.status).toEqual("error");
     });
 
 });
