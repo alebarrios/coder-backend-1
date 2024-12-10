549ddb7d0ca4f53d66b8446b0eecd4549b128535
Alejandro <al.barrios88@gmail.com>
2024-12-10 19:22:31 -0300

    Mod CartManager.js con cart.model.js

--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -1,99 +1,62 @@
 import ErrorManager from "./ErrorManager.js";
-import { readJsonFile, writeJsonFile } from "../utils/fileHandler.js"
-import paths from "../utils/paths.js";
-import generateId from "../utils/collectionHandler.js"
+import { isValidID } from "../config/mongoose.config.js";
+import CartModel from "../models/cart.model.js";
 
 export default class CartManager {
-    #jsonFilename;
-    #carts;
+    #cartModel;
 
     constructor() {
-        this.#jsonFilename = "carrito.json";
+        this.#cartModel = CartModel;
     }
 
     // Busca un carrito por su ID
     async #findOneById(id) {
 
-        if(isNaN(id)) {
-            throw new ErrorManager("ID no numérico", 400);
+        if (!isValidID(id)) {
+            throw new ErrorManager("ID inválido", 400);
         }
-        this.#carts = await this.getAll();
-        const productFound = this.#carts.find((item) => item.id === Number(id));
 
-        if (!productFound) {
+        const cartFound = await this.#cartModel.findById(id).populate("products.product");
+
+        if (!cartFound) {
             throw new ErrorManager("ID no encontrado", 404);
         }
 
-        return productFound;
+        return cartFound;
     }
 
     // Obtiene una lista de carritos
-    async getAll() {
+    async getAll(params) {
         try {
-            this.#carts = await readJsonFile(paths.files, this.#jsonFilename);
-            return this.#carts;
+            const paginationOptions = {
+                limit: params?.limit || 10, // Número de documentos por página (por defecto 10)
+                page: params?.page || 1, // Página actual (por defecto 1)
+                populate: "products.product", // Poblar el campo virtual 'products'
+                lean: true, // Convertir los resultados en objetos planos
+            };
+
+            return await this.#cartModel.paginate({}, paginationOptions);
         } catch (error) {
-            throw new ErrorManager(error.message, error.code);
+            throw ErrorManager.handleError(error);
         }
     }
 
     // Obtiene un carrito específico por su ID
     async getOneById(id) {
         try {
-            const productFound = await this.#findOneById(id);
-            return productFound;
+            return await this.#findOneById(id);
         } catch (error) {
-            throw new ErrorManager(error.message, error.code);
+            throw ErrorManager.handleError(error);
         }
     }
 
     // Inserta un carrito con productos
     async insertOne(data) {
         try {
-
-            if (data == undefined || !('products' in data) || !Array.isArray(data.products)
-            || !(data.products.length > 0)) {
-                throw new ErrorManager("Faltan datos obligatorios", 400);
-            }
-
-            if(Object.keys(data).length > 1){
-                throw new ErrorManager("El request tiene formato inválido", 400);
-            }
-
-            const newProducts = [];
-            data.products.forEach(item => {
-                if(Object.keys(item).length > 2){
-                    throw new ErrorManager("products tiene formato inválido", 400);
-                }
-                if(!('productId' in item) || !('quantity' in item)){
-                    throw new ErrorManager("Faltan datos obligatorios", 400);
-                }
-                if( isNaN(item.productId)){
-                    throw new ErrorManager("productId debe ser numérico", 400);
-                }
-                if( isNaN(item.quantity) || item.quantity < 0){
-                    throw new ErrorManager("quantity debe ser numérico y no negativo", 400);
-                }
-
-                const productFound = newProducts.find((item2) => item2.productId === Number(item.productId));
-                if(!productFound) {
-                    newProducts.push({productId: item.productId, quantity: item.quantity});
-                }else{
-                    throw new ErrorManager("productId duplicado", 400);
-                }
-            });
-
-            const cart = {
-                id: generateId(await this.getAll()),
-                products: newProducts
-            };
-
-            this.#carts.push(cart);
-            await writeJsonFile(paths.files, this.#jsonFilename, this.#carts);
-
+            const cart = await this.#cartModel.create(data);
             return cart;
         } catch (error) {
-            throw new ErrorManager(error.message, error.code);
+            throw ErrorManager.handleError(error);
         }
     }
 
@@ -101,26 +64,20 @@ export default class CartManager {
     async addProductToCart(id, productId) {
         try {
             const cartFound = await this.#findOneById(id);
-
-            if( isNaN(productId) || productId < 0){
-                throw new ErrorManager("productId debe ser numérico y no negativo", 400);
-            }
-
-            const productIndex = cartFound.products.findIndex((item) => item.productId === Number(productId));
+            const productIndex =
+                recipe.products.findIndex((item) => item.product._id.toString() === productId);
 
             if (productIndex >= 0) {
-                cartFound.products[productIndex].quantity++;
+                recipe.products[productIndex].quantity++;
             } else {
-                cartFound.products.push({ productId: Number(productId), quantity: 1 });
+                recipe.products.push({ product: productId, quantity: 1 });
             }
 
-            const index = this.#carts.findIndex((item) => item.id === Number(id));
-            this.#carts[index] = cartFound;
-            await writeJsonFile(paths.files, this.#jsonFilename, this.#carts);
+            await recipe.save();
 
             return cartFound;
         } catch (error) {
-            throw new ErrorManager(error.message, error.code);
+            throw ErrorManager.handleError(error);
         }
     }
 
