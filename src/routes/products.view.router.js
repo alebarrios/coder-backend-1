aaa7c56065418f5395612a414a7751fa522ec7f2
Alejandro <al.barrios88@gmail.com>
2024-12-14 01:46:09 -0300

    modificaciones en el front view /products/:id

--- src/routes/products.view.router.js
+++ src/routes/products.view.router.js
@@ -4,11 +4,24 @@ import ProductManager from "../managers/ProductManager.js";
 const productsViewRouter = Router();
 const productManager = new ProductManager();
 
-productsViewRouter.get("/", async (req, res) => {
+productsViewRouter.get("/products", async (req, res) => {
     try {
         const products = await productManager.getAll();
         res.status(200)
-        .render("home", { layout : 'index', style: 'index.css', js: 'index.js', title: "Productos", products });
+        .render("home", { layout : 'index', style: 'index.css', js: 'index.js', title: "Productos", products: products.docs });
+    } catch (error) {
+        res.status(error.code || 500).send(`<h1>Error</h1><h3>${error.message}</h3>`);
+    }
+});
+
+productsViewRouter.get("/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await productManager.getOneById(id);
+        console.log(product);
+        
+        res.status(200)
+        .render("productDetail", { layout : 'index', style: 'index.css', js: 'productDetail.js', id, product: product.toJSON() });
     } catch (error) {
         res.status(error.code || 500).send(`<h1>Error</h1><h3>${error.message}</h3>`);
     }
